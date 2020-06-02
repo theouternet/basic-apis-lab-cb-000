d22cfc9854f6ac694d1a85bd3cf53188ff9f744f
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-06-02 06:27:19 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -6,7 +6,7 @@ class RepositoriesController < ApplicationController
 
   def github_search
         client_id = "8e52ba79d99a414b8d61"
-    secret = "REPLACE ME"
+    secret = "461c7a065b83e011188df751550cbb742035dfea"
 
     resp = Faraday.get("https://api.github.com/search/repositories") do |req|
       req.params["q"] = params[:query]
